739fc3dbb965aa8d21fe7dd8cadc19f8f8a17de0
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-05-19 16:13:29 -0300

    Agregar archivos de proyecto.

---
+++ Data/NonCompliantClients.cs
@@ -0,0 +1,11 @@
+﻿namespace Arcane.Itec.Data
+{
+    public class NonCompliantClients
+    {
+        public string Id { get; set; }
+        public string WalkerName { get; set; }
+        public string ClientName { get; set; }
+        public string Type { get; set; }
+        public string Sale { get; set; }
+    }
+}
