169f273c3fcb3e2cdd53475f2c5cc497ef08f9c4
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-05-25 12:38:42 -0300

    Refactoring CalculateSimReward

--- SalaryCalculator.cs
+++ SalaryCalculator.cs
@@ -11,11 +11,7 @@ namespace Arcane.Itec
     {
         private readonly CommissionValue Commission;
         private readonly CommisionRules Rules;
-        private int TotalSimReward { get; set; }
-        private int TotalSelloutReward { get; set; }
         private int TotalVolumes { get; set; }
-        private int TotalVolumeRewards { get; set; }
-        private int TotalEmployeeSalarys { get; set; }
 
         public SalaryCalculator(CommissionValue commission, CommisionRules rules)
         {
@@ -58,23 +54,6 @@ namespace Arcane.Itec
                 e.TotalSalary = CalculateTotalRewards(e);
             });
 
-            var totalSimAmount = agencyPsr.Where(p => p.Value.SimOk).Count();
-            var totalSelloutAmount = agencyPsr.Where(p => p.Value.SelloutOk).Count();
-
-            employees.Add(new Employee
-            {
-                Name = "TOTALES",
-                PsrAmount = agencyPsr.Values.Count,
-                SimAmount = totalSimAmount,
-                SimReward = "$ " + TotalSimReward,
-                SimPercentage = Utils.GetEfectivity(agencyPsr.Values.Count, totalSimAmount),
-                SelloutAmount = totalSelloutAmount,
-                SelloutReward = "$ " + TotalSelloutReward,
-                SelloutPercentage = Utils.GetEfectivity(agencyPsr.Values.Count, totalSelloutAmount),
-                VolumeAmount = TotalVolumes,
-                VolumeReward = "$ " + TotalVolumeRewards,
-                TotalSalary = "$ " + TotalEmployeeSalarys
-            });
             return employees;
         }
 
@@ -89,17 +68,15 @@ namespace Arcane.Itec
             });
         }
 
-        private string CalculateSimReward(Employee employee)
+        private int CalculateSimReward(Employee employee)
         {
             var commision = Commission.DefaultSim;
+
             if (employee.SimAmount >= Rules.SimStep3) commision = Commission.SimStep3;
             else if (employee.SimAmount >= Rules.SimStep2) commision = Commission.SimStep2;
             else if (employee.SimAmount >= Rules.SimStep1) commision = Commission.SimStep1;
 
-            var reward = employee.SimAmount * commision;
-            TotalSimReward += reward;
-            TotalEmployeeSalarys += reward;
-            return "$ " + reward;
+            return employee.SimAmount * commision;
         }
 
         private string CalculateSOReward(Employee employee)
@@ -109,8 +86,6 @@ namespace Arcane.Itec
             else if (employee.SelloutAmount >= Rules.SelloutStep1) commision = Commission.SelloutStep1;
 
             var reward = employee.SelloutAmount * commision;
-            TotalSelloutReward += reward;
-            TotalEmployeeSalarys += reward;
             return "$ " + reward;
         }
 
@@ -120,9 +95,6 @@ namespace Arcane.Itec
 
             long multiplyResult = (long)employee.VolumeAmount * Commission.VolumePayment;
             var reward = (int)Math.Round((double)multiplyResult / volume);
-
-            TotalVolumeRewards += reward;
-            TotalEmployeeSalarys += reward;
             return "$ " + reward;
         }
 
